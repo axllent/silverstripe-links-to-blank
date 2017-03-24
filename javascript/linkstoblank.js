743b0b2bafbb6ccc116ee8c1fa0e18c7c8d2168c
Ralph Slooten <ralph@technojoy.co.nz>
2017-03-24 23:28:38 +1300

    Sorter code, fix bug

--- javascript/linkstoblank.js
+++ javascript/linkstoblank.js
@@ -1,18 +1,14 @@
 function _l2b() {
-    var a = document.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        if (!a[i].target && (a[i].href.indexOf(location.host) == -1 && a[i].href.match(/^https?\:\/\//i)) ||
-            a[i].href.match(/\.(pdf|docx?|pp(s|tx?)|xlsx?|zip|gz|bz2|(r|t)ar|7z)$/i)
+    var l = document.getElementsByTagName("a");
+    for (let a of l) {
+        if (a.href && !a.target && (a.href.indexOf(location.host) == -1 && a.href.match(/^https?\:\/\//i)) ||
+            a.href.match(/\.(pdf|docx?|pp(s|tx?)|xlsx?|zip|gz|bz2|(r|t)ar|7z)$/i)
         ) {
-            a[i].target = "_blank";
-            if (a[i].rel && !a[i].rel.match(/noopener/i)) {
-                a[i].rel += " noopener";
-            } else {
-                a[i].rel = "noopener";
-            }
+            a.target = "_blank";
+            a.rel += a.rel ? !a.rel.match(/noopener/i) ? " noopener" : '' : 'noopener';
         }
     }
 }
 
 var w = window;
-w.addEventListener ? w.addEventListener("load", _l2b, !1) : w.attachEvent && w.attachEvent("onload", _l2b);
+w.addEventListener ? w.addEventListener("load",_l2b,!1) : w.attachEvent && w.attachEvent("onload",_l2b);
