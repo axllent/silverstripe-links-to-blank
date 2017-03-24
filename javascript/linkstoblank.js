ad8d35b5bcba7f7e1f6df8fd7e5b85bc6e29fa4f
Ralph Slooten <ralph@technojoy.co.nz>
2017-03-24 23:28:38 +1300

    Shorter code, fix bug

--- javascript/linkstoblank.js
+++ javascript/linkstoblank.js
@@ -1,16 +1,11 @@
-function _l2b(){
-	var a = document.getElementsByTagName("a");
-	for(i = 0; i<a.length; i++) {
-		if(
-            !a[i].target && (a[i].href.indexOf(location.host) == -1 && a[i].href.match(/^https?\:\/\//i))
-			|| a[i].href.match(/\.(pdf|docx?|pp(s|tx?)|xlsx?|zip|gz|bz2|(r|t)ar|7z)$/i)
+function _l2b() {
+    var l = document.getElementsByTagName("a");
+    for (let a of l) {
+        if (a.href && !a.target && (a.href.indexOf(location.host) == -1 && a.href.match(/^https?\:\/\//i)) ||
+            a.href.match(/\.(pdf|docx?|pp(s|tx?)|xlsx?|zip|gz|bz2|(r|t)ar|7z)$/i)
         ) {
-				a[i].target = "_blank";
-				if (a[i].rel && !a[i].rel.match(/noopener/i)) {
-                    a[i].rel += " noopener";
-                } else {
-                    a[i].rel = "noopener";
-                }
+            a.target = "_blank";
+            a.rel += a.rel ? !a.rel.match(/noopener/i) ? " noopener" : '' : 'noopener';
         }
     }
 }
